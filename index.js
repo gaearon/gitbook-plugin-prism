ec74364ce9ed46289862de073b51ef6f49405436
Samy Pessé <samypesse@gmail.com>
2014-04-19 19:39:26 +0200

    Add example hooks

--- index.js
+++ index.js
@@ -6,5 +6,13 @@ module.exports = {
         css: [
             "book/test.css"
         ]
+    },
+    hooks: {
+        init: function() {
+            console.log("init!");
+        },
+        finish: function() {
+            console.log("finish!");
+        }
     }
 };
