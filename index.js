da4d4ab48902229b4b21cabf09569ecae4da0487
Samy Pessé <samypesse@gmail.com>
2014-04-28 17:20:37 +0200

    Add more informations about hooks and add hook "page"

--- index.js
+++ index.js
@@ -9,11 +9,23 @@ module.exports = {
         ]
     },
     hooks: {
+        // For all the hooks, this represent the current generator
+
+        // This is called before the book is generated
         init: function() {
             console.log("init!");
         },
+
+        // This is called after the book generation
         finish: function() {
             console.log("finish!");
+        },
+
+        // This is called for each page of the book
+        // It can be used for modifing page content
+        // It should return the new page
+        page: function(page) {
+            return page;
         }
     }
 };
