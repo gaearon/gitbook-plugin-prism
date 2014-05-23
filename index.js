8edfcb707a15de8ed420a371015c607a9178c9a9
Samy Pessé <samypesse@gmail.com>
2014-05-23 16:59:16 +0200

    Improve example for html snippets

--- index.js
+++ index.js
@@ -8,8 +8,16 @@ module.exports = {
             "test.css"
         ],
         html: {
+            "html:start": function() {
+                return "<!-- Start book "+this.options.title+" -->"
+            },
+            "html:end": function() {
+                return "<!-- End of book "+this.options.title+" -->"
+            },
+
             "head:start": "<!-- head:start -->",
             "head:end": "<!-- head:end -->",
+
             "body:start": "<!-- body:start -->",
             "body:end": "<!-- body:end -->"
         }
