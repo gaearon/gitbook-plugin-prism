9ffabd2647ae38bd7fa6622abfbb2203bc975fe5
崎轩 <chuncheng.licc@alibaba-inc.com>
2015-03-19 11:28:59 +0800

    modify README.md

--- README.md
+++ README.md
@@ -1,41 +1,21 @@
-GitBook Sample Plugin
+GitBook Google Code Prettify Plugin
 ==============
 
-This is a model for GitBook plugins.
-
-## How GitBook plugin works?
-
-A plugin for GitBook is a node package that can be published on [NPM](http://www.npmjs.org). It has to follow the name convention: `gitbook-plugin-*name*`.
 
 ### package.json
 
 #### name
 
-The package name should begin with ```gitbook-plugin-```.
+The package name should set ```gitbook-plugin-google_code_prettify```.
 
-Examples: `gitbook-plugin-mixpanel`, `gitbook-plugin-googleanalytics`.
+#### How to use ?
 
-#### engine
+Step1. book.json configure
 
-The package.json should contain a `engine` field using [the standard norm](https://www.npmjs.org/doc/json.html#engines).
-
-```
-"engines": {
-    "gitbook": "*"
+{
+    "plugins": ["gitbookcode"]
 }
-```
 
-For example if you want your plugin to supports only GitBook version supperior to 0.3.1:
+Step2. book.json configure
 
-```
-"engines": {
-    "gitbook": ">=0.3.1"
-}
-```
-
-### entry point
-
-The plugin entry point should return an object with some metadata.
-
-
-# google_code_prettify
+gitbook build ./
--- index.js
+++ index.js
@@ -1,84 +1,19 @@
 module.exports = {
-    // Extend website resources and html
-    website: {
+    book: {
         assets: "./book",
         js: [
-            "test.js"
+            "google-code-prettify.js",
+            "plugin.js"
         ],
         css: [
-            "test.css"
-        ],
-        html: {
-            "html:start": function() {
-                return "<!-- Start book "+this.options.title+" -->"
-            },
-            "html:end": function() {
-                return "<!-- End of book "+this.options.title+" -->"
-            },
-
-            "head:start": "<!-- head:start -->",
-            "head:end": "<!-- head:end -->",
-
-            "body:start": "<!-- body:start -->",
-            "body:end": "<!-- body:end -->"
-        }
+            "google-code-prettify.css"
+        ]
     },
-
-    // Extend ebook resources and html
-    website: {
-        assets: "./book",
-        js: [
-            "test.js"
-        ],
-        css: [
-            "test.css"
-        ],
-        html: {
-            "html:start": function() {
-                return "<!-- Start book "+this.options.title+" -->"
-            },
-            "html:end": function() {
-                return "<!-- End of book "+this.options.title+" -->"
-            },
-
-            "head:start": "<!-- head:start -->",
-            "head:end": "<!-- head:end -->",
-
-            "body:start": "<!-- body:start -->",
-            "body:end": "<!-- body:end -->"
-        }
-    },
-
-    // Extend templating blocks
     blocks: {
-        // Author will be able to write "{% myTag %}World{% endMyTag %}"
-        myTag: {
+        code: {
             process: function(blk) {
-                return "Hello "+blk.body;
+
             }
-        }
-    },
-
-    // Extend templating filters
-    filters: {
-        // Author will be able to write "{{ 'test'|myFilter }}"
-        myFilter: function(s) {
-            return "Hello "+s;
-        }
-    },
-
-    // Hook process during build
-    hooks: {
-        // For all the hooks, this represent the current generator
-
-        // This is called before the book is generated
-        "init": function() {
-            console.log("init!");
-        },
-
-        // This is called after the book generation
-        "finish": function() {
-            console.log("finish!");
         }
     }
 };
